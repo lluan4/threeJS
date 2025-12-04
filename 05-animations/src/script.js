2b6a479016a76635720f914e9eda61b700755f78
LuanLima <lluan.lima94@gmail.com>
2025-12-04 11:01:47 -0300

    feat: Install core project dependencies including Three.js, GSAP, and build tooling.

---
+++ 05-animations/src/script.js
@@ -0,0 +1,65 @@
+import * as THREE from 'three'
+import gsap from 'gsap'
+
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+
+//Time
+// let time = Date.now()
+// const clock = new THREE.Clock()
+gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 })
+gsap.to(mesh.position, { duration: 1, delay: 3, x: -2 })
+
+// Animations
+const tick = () => {
+
+    //Time
+    // const current = Date.now()
+    // const delta = current - time
+    // time = current
+    // const elapsedTime = clock.getElapsedTime()
+    
+    //Update objects
+    // mesh.rotation.x += 0.001 * delta
+    // mesh.rotation.y += 0.001 * delta
+    // camera.position.x =  Math.sin(elapsedTime)
+    // camera.position.y = Math.cos(elapsedTime)
+    // camera.lookAt(mesh.position)
+
+    
+    //Render
+    renderer.render(scene, camera)
+    
+    window.requestAnimationFrame(tick)
+}
+
+tick()
+
+
