d447ec3d0b6aae0b503addb1d582aeae57240cdb
Luan Lima <lluan.lima94@gmail.com>
2025-12-15 10:04:39 -0300

    lesson 35

--- 35-lights-shading-shaders/src/shaders/shading/fragment.glsl
+++ 35-lights-shading-shaders/src/shaders/shading/fragment.glsl
@@ -1,8 +1,16 @@
 uniform vec3 uColor;
 
-vec3 ambientLight(vec3 lightColor, float lightIntensity){
+varying vec3 vNormal;
 
-    return lightColor * lightIntensity;
+#include ../includes/ambientLight.glsl
+
+vec3 directionalLight(vec3 lightColor, float lightIntensity, vec3 normal, vec3 lightPosition){
+    vec3 lightDir = normalize(lightPosition);
+
+    // Shading
+    float shading = dot(normal, lightDir);
+
+    return lightColor * lightIntensity * shading;
 }
 
 void main()
@@ -11,7 +19,16 @@ void main()
 
     // Light
     vec3 light = vec3(0.0);
-    light += ambientLight(vec3(1.0), 0.2);
+    // light += ambientLight(
+    //     vec3(1.0),
+    //     0.2
+    // );
+    light += directionalLight(
+        vec3(0.1, 0.1, 1.0),
+        1.0,
+        vNormal,
+        vec3(0.0, 0.0, 3.0)
+    );
     color *= light;
 
     // Final color
