39fde7302e42befd40bb17f50903b700318f70e0
Luan Lima <lluan.lima94@gmail.com>
2025-12-22 17:19:43 -0300

    lesson 40

--- 40-particles-morphing-shader/src/script.js
+++ 40-particles-morphing-shader/src/script.js
@@ -4,7 +4,6 @@ import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import { DRACOLoader } from "three/addons/loaders/DRACOLoader.js";
 import GUI from "lil-gui";
 import gsap from "gsap";
-console.log("ok");
 import particlesVertexShader from "./shaders/particles/vertex.glsl";
 import particlesFragmentShader from "./shaders/particles/fragment.glsl";
 
@@ -43,10 +42,11 @@ window.addEventListener("resize", () => {
   sizes.pixelRatio = Math.min(window.devicePixelRatio, 2);
 
   // Materials
-  particles.material.uniforms.uResolution.value.set(
-    sizes.width * sizes.pixelRatio,
-    sizes.height * sizes.pixelRatio
-  );
+  if (particles)
+    particles.material.uniforms.uResolution.value.set(
+      sizes.width * sizes.pixelRatio,
+      sizes.height * sizes.pixelRatio
+    );
 
   // Update camera
   camera.aspect = sizes.width / sizes.height;
@@ -94,32 +94,151 @@ renderer.setClearColor(debugObject.clearColor);
 /**
  * Particles
  */
-const particles = {};
+let particles = null;
+// Load models
+gltfLoader.load("./models.glb", (gltf) => {
+  particles = {};
+  particles.index = 0;
 
-// Geometry
-particles.geometry = new THREE.SphereGeometry(3);
-particles.geometry.setIndex(null);
+  // Positions
+  const positions = gltf.scene.children.map(
+    (child) => child.geometry.attributes.position
+  );
 
-// Material
-particles.material = new THREE.ShaderMaterial({
-  vertexShader: particlesVertexShader,
-  fragmentShader: particlesFragmentShader,
-  uniforms: {
-    uSize: new THREE.Uniform(0.4),
-    uResolution: new THREE.Uniform(
-      new THREE.Vector2(
-        sizes.width * sizes.pixelRatio,
-        sizes.height * sizes.pixelRatio
-      )
-    ),
-  },
-  blending: THREE.AdditiveBlending,
-  depthWrite: false,
+  particles.maxCount = 0;
+  for (const position of positions) {
+    if (position.count > particles.maxCount)
+      particles.maxCount = position.count;
+  }
+
+  particles.positions = [];
+  for (const position of positions) {
+    const originalArray = position.array;
+    const newArray = new Float32Array(particles.maxCount * 3);
+
+    for (let i = 0; i < newArray.length; i++) {
+      const i3 = i * 3;
+
+      if (i3 < originalArray.length) {
+        newArray[i3 + 0] = originalArray[i3 + 0];
+        newArray[i3 + 1] = originalArray[i3 + 1];
+        newArray[i3 + 2] = originalArray[i3 + 2];
+      } else {
+        const randomIndex = Math.floor(position.count * Math.random()) * 3;
+
+        newArray[i3 + 0] = originalArray[randomIndex + 0];
+        newArray[i3 + 1] = originalArray[randomIndex + 1];
+        newArray[i3 + 2] = originalArray[randomIndex + 2];
+      }
+    }
+    particles.positions.push(new THREE.Float32BufferAttribute(newArray, 3));
+  }
+
+  // Geometry
+  const sizesArray = new Float32Array(particles.maxCount);
+
+  for (let i = 0; i < particles.maxCount; i++) {
+    sizesArray[i] = Math.random();
+  }
+
+  particles.geometry = new THREE.BufferGeometry();
+  particles.geometry.setAttribute(
+    "position",
+    particles.positions[particles.index]
+  );
+  particles.geometry.setAttribute("aPositionTarget", particles.positions[3]);
+  particles.geometry.setAttribute(
+    "aSizes",
+    new THREE.Float32BufferAttribute(sizesArray, 1)
+  );
+  particles.geometry.setIndex(null);
+
+  // Material
+  particles.colorA = "#ff7300";
+  particles.colorB = "#0091ff";
+  particles.material = new THREE.ShaderMaterial({
+    vertexShader: particlesVertexShader,
+    fragmentShader: particlesFragmentShader,
+    uniforms: {
+      uSize: new THREE.Uniform(0.4),
+      uResolution: new THREE.Uniform(
+        new THREE.Vector2(
+          sizes.width * sizes.pixelRatio,
+          sizes.height * sizes.pixelRatio
+        )
+      ),
+      uProgress: new THREE.Uniform(0.0),
+      uColorA: new THREE.Uniform(new THREE.Color(particles.colorA)),
+      uColorB: new THREE.Uniform(new THREE.Color(particles.colorB)),
+    },
+    blending: THREE.AdditiveBlending,
+    depthWrite: false,
+  });
+
+  // Methods
+  particles.morph = (index) => {
+    // Update attributes
+    particles.geometry.attributes.position =
+      particles.positions[particles.index];
+    particles.geometry.attributes.aPositionTarget = particles.positions[index];
+
+    // Animate uProgress
+    gsap.fromTo(
+      particles.material.uniforms.uProgress,
+      {
+        value: 0,
+      },
+      {
+        value: 1,
+        duration: 3,
+        ease: "linear",
+      }
+    );
+
+    // Save ubdex
+    particles.index = index;
+  };
+
+  particles.morph0 = () => {
+    particles.morph(0);
+  };
+  particles.morph1 = () => {
+    particles.morph(1);
+  };
+  particles.morph2 = () => {
+    particles.morph(2);
+  };
+  particles.morph3 = () => {
+    particles.morph(3);
+  };
+
+  // Tweaks
+  gui
+    .add(particles.material.uniforms.uProgress, "value")
+    .min(0)
+    .max(1)
+    .step(0.001)
+    .name("uProgress")
+    .listen();
+
+  gui.addColor(particles, "colorA").onChange(() => {
+    particles.material.uniforms.uColorA.value.set(particles.colorA);
+  });
+
+  gui.addColor(particles, "colorB").onChange(() => {
+    particles.material.uniforms.uColorB.value.set(particles.colorB);
+  });
+
+  gui.add(particles, "morph0").name("Morph to 0");
+  gui.add(particles, "morph1").name("Morph to 1");
+  gui.add(particles, "morph2").name("Morph to 2");
+  gui.add(particles, "morph3").name("Morph to 3");
+
+  // Points
+  particles.points = new THREE.Points(particles.geometry, particles.material);
+  particles.points.frustumCulled = false;
+  scene.add(particles.points);
 });
-
-// Points
-particles.points = new THREE.Points(particles.geometry, particles.material);
-scene.add(particles.points);
 
 /**
  * Animate
@@ -127,6 +246,8 @@ scene.add(particles.points);
 const tick = () => {
   // Update controls
   controls.update();
+
+  //Update progress
 
   // Render normal scene
   renderer.render(scene, camera);
